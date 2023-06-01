ce92c0221d52e6dc55ce353d9327ab0a32776dd8
K1ose <klose@jk404.cn>
2023-06-02 05:26:04 +0800

    update

---
+++ hello_cargo/target/debug/deps/hello_cargo-91fc84a64fdb2d03.d
@@ -0,0 +1,5 @@
+D:\project\rust_tutorial\hello_cargo\target\debug\deps\hello_cargo-91fc84a64fdb2d03.rmeta: src\main.rs
+
+D:\project\rust_tutorial\hello_cargo\target\debug\deps\hello_cargo-91fc84a64fdb2d03.d: src\main.rs
+
+src\main.rs:
