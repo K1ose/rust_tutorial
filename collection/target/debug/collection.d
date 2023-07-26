a7a21aa980688ec09dcaffc90bb9bc7ee8a9ac00
K1ose <klose@jk404.cn>
2023-07-26 17:23:40 +0800

    update

---
+++ collection/target/debug/collection.d
@@ -0,0 +1,1 @@
+/data/cmcc/project/Rust/rust_tutorial/collection/target/debug/collection: /data/cmcc/project/Rust/rust_tutorial/collection/src/main.rs
---
+++ collection/target/debug/deps/collection-c3751e23c81f2ce3.d
@@ -0,0 +1,5 @@
+/data/cmcc/project/Rust/rust_tutorial/collection/target/debug/deps/collection-c3751e23c81f2ce3: src/main.rs
+
+/data/cmcc/project/Rust/rust_tutorial/collection/target/debug/deps/collection-c3751e23c81f2ce3.d: src/main.rs
+
+src/main.rs:
